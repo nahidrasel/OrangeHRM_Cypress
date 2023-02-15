c05d1adf804c57a65fa2dbb8c917937bb157d7c9
Nahid Mahmud <nhdrasel@gmail.com>
2023-02-16 00:22:37 +1300

    Added Reporting

--- cypress.config.js
+++ cypress.config.js
@@ -8,22 +8,11 @@ module.exports = defineConfig({
   defaultCommandTimeout: 5000,
   viewportWidth: 1000,
   viewportHeight: 600,
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/reports',
-    charts: true,
-    embeddedScreenshots: true,
-    html: true,
-    inlineAssets: true,
-    overwrite: false,
-    json: false
-  },
   video: false,
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
